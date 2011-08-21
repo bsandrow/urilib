a476bb88469f5ec4c2c790a468933f2ccb072002
Brandon Sandrowicz <bsandrow@gmail.com>
2011-08-21 14:57:55 -0700

    Update

--- urilib/__init__.py
+++ urilib/__init__.py
@@ -1,50 +1,88 @@
 import re
 
-scheme_re = re.compile(r'[a-z][a-z0-9+\-.]*', re.I)
+scheme_re   = re.compile(r'\w[\w0-9+\-.]*', re.IGNORECASE | re.UNICODE)
+fragment_re = re.compile(r'''([\w\d\-._~!$&'()*+,;=/?:@]|%[a-z0-9]{2})*''', re.IGNORECASE | re.UNICODE)
+query_re    = re.compile(r'''([\w\d\-._~!$&'()*+,;=/?:@]|%[a-z0-9]{2})*''', re.IGNORECASE | re.UNICODE)
 
 class URI(object):
 
     def __init__(self, uri, scheme=None):
         self.uri = uri
-
-        # Initially decompose via:
-        #   <scheme>:<scheme-specific-part>
-        self.scheme = scheme
-        self._decompose_uri()
-        if self.scheme is None:
-            raise URIParseError('Could not determine scheme for URI')
+        self._decompose_uri(scheme=scheme)
 
     @classmethod
-    def verify_scheme(cls, scheme):
+    def is_valid_scheme(cls, scheme):
         '''
-        Verify that the scheme meets the following spec (from RFC 2396):
-            scheme   = alpha *( alpha | digit | "+" | "-" | "." )
-
-            digit    = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" |
-                       "8" | "9"
-
-            alpha    = lowalpha | upalpha
-
-            lowalpha = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" |
-                       "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" |
-                       "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
-
-            upalpha  = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" |
-                       "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" |
-                       "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
-
+        Verify that the scheme meets the following spec (from RFC 3986):
+            scheme      = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
         '''
         return scheme_re.match(scheme) is not None
 
-    def _decompose_uri(self):
-        ''' Decompose a URI into its most basic top-level components via the
-        following definition in RFC 2396:
-            <scheme>:<scheme-specific-part> '''
-        parts = self.uri.split(':', 1)
-        if len(parts) > 1 and URI.verify_scheme(parts[0]):
-            (self.scheme, self.scheme_specific_part) = parts
+    @classmethod
+    def is_valid_fragment(cls, fragment):
+        '''
+        Verify that the fragment meets the following spec (from RFC 3986):
+            fragment      = *( pchar / "/" / "?" )
+            pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+            pct-encoded   = "%" HEXDIG HEXDIG
+            unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+            sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+                          / "*" / "+" / "," / ";" / "="
+        '''
+        return fragment_re.match(fragment) is not None
+
+    @classmethod
+    def is_valid_query(cls, query):
+        return query_re.match(query) is not None
+
+    def _decompose_uri(self, scheme=None):
+        ''' Decompose the URI into it's component parts according to RFC 3986:
+                URI           = scheme ":" hier-part [ "?" query ] [ "#" fragment ]
+                hier-part     = "//" authority path-abempty
+                              / path-absolute
+                              / path-rootless
+                              / path-empty
+                path          = path-abempty    ; begins with "/" or is empty
+                              / path-absolute   ; begins with "/" but not "//"
+                              / path-noscheme   ; begins with a non-colon segment
+                              / path-rootless   ; begins with a segment
+                              / path-empty      ; zero characters
+                path-abempty  = *( "/" segment )
+                path-absolute = "/" [ segment-nz *( "/" segment ) ]
+                path-noscheme = segment-nz-nc *( "/" segment )
+                path-rootless = segment-nz *( "/" segment )
+                path-empty    = 0<pchar>
+                segment       = *pchar
+                segment-nz    = 1*pchar
+                segment-nz-nc = 1*( unreserved / pct-encoded / sub-delims / "@" )
+                              ; non-zero-length segment without any colon ":"
+
+                pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+        '''
+        buffer = self.uri
+        if scheme is None:
+            parts = buffer.split(u':', 1)
+            if len(parts) > 1 and URI.is_valid_scheme(parts[0]):
+                (self.scheme, buffer) = parts
         else:
-            self.scheme_specific_part = self.uri
+            self.scheme = scheme
+
+        if buffer.startswith('//'):
+            # extract the authority part
+
+        parts = buffer.split('?', 1)
+        if len(parts)> 1 and URI.is_valid_query(parts[1]):
+            pass
+
+        # deal with the path part
+
+        parts = buffer.split('#', 1)
+        if len(parts) > 1 and URI.is_valid_fragment(parts[1]):
+            (self.fragment, buffer) = parts
+
+
+
+
 
     def __str__(self):
         ''' Return the full URI '''
