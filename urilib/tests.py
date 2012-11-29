ed819adc17755621ac0b13392711e57de0cf1e82
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-11-29 00:06:24 -0500

    Cleaning up the tests

--- urilib/tests.py
+++ urilib/tests.py
@@ -2,60 +2,58 @@ import unittest
 
 from urilib.uri import URI
 
-class SchemeParsing(unittest.TestCase):
-    def test_abs_url_with_port(self):
+class UriParsing(unittest.TestCase):
+    def test_scheme_abs_url_with_port(self):
         uri = URI('http://example.com:80')
-        assert uri.scheme == 'http'
+        self.assertEquals(uri.scheme, 'http')
 
-    def test_relative_url_with_port(self):
+    def test_scheme_relative_url_with_port(self):
         uri = URI('//example.com:80')
-        assert uri.scheme is None
+        self.assertEquals(uri.scheme, None)
 
-    def test_invalid_url(self):
+    def test_scheme_invalid_url(self):
         uri = URI('example.com:80')
-        assert uri.scheme == 'example.com'
+        self.assertEquals(uri.scheme, 'example.com')
 
-    def test_urn(self):
+    def test_scheme_urn(self):
         uri = URI('urn:example:monkey:toes')
-        assert uri.scheme == 'urn'
+        self.assertEquals(uri.scheme, 'urn')
 
-    def test_magnet_uri(self):
+    def test_scheme_magnet_uri(self):
         uri = URI('magnet:?xt=urn:md5:ABCDEACNDE')
-        assert uri.scheme == 'magnet'
+        self.assertEquals(uri.scheme, 'magnet')
 
     def test_scheme_name_with_dash(self):
         uri = URI('scheme-name://example.com')
-        assert uri.scheme == 'scheme-name'
+        self.assertEquals(uri.scheme, 'scheme-name')
 
     def test_scheme_name_with_plus(self):
         uri = URI('scheme+name://example.com')
-        assert uri.scheme == 'scheme+name'
+        self.assertEquals(uri.scheme, 'scheme+name')
 
     def test_scheme_name_with_dot(self):
         uri = URI('scheme.name://example.com')
-        assert uri.scheme == 'scheme.name'
+        self.assertEquals(uri.scheme, 'scheme.name')
 
-class FragmentParsing(unittest.TestCase):
     def test_fragment(self):
         uri = URI('http://example.com?var1=value&var2=Value2#var3=$value')
-        assert uri.fragment == 'var3=$value'
+        self.assertEquals(uri.fragment, 'var3=$value')
 
     def test_fragment_double_hash(self):
         uri = URI('http://example.com?var1=value&var2=Value2#me-and-my-#fragment')
-        assert uri.fragment == 'me-and-my-#fragment'
+        self.assertEquals(uri.fragment, 'me-and-my-#fragment')
 
-class QueryParsing(unittest.TestCase):
     def test_query(self):
         uri = URI('http://example.com?q=how+do+i+program+my+vcr')
-        assert str(uri.query) == 'q=how+do+i+program+my+vcr'
+        self.assertEquals(str(uri.query), 'q=how+do+i+program+my+vcr')
 
     def test_query_qmark_in_query(self):
         uri = URI('http://example.com?q=how+do+i+program+my+vcr?')
-        assert str(uri.query) == 'q=how+do+i+program+my+vcr?'
+        self.assertEquals(str(uri.query), 'q=how+do+i+program+my+vcr?')
 
     def test_query_qmark_in_fragment(self):
         uri = URI('http://example.com?q=how+do+i+program+my+vcr?#why-do-i-care?')
-        assert str(uri.query) == 'q=how+do+i+program+my+vcr?'
+        self.assertEquals(str(uri.query), 'q=how+do+i+program+my+vcr?')
 
     def test_query_no_query(self):
         uri = URI('http://example.com/#Fragment')
@@ -63,13 +61,12 @@ class QueryParsing(unittest.TestCase):
 
     def test_query_empty_query(self):
         uri = URI('http://example.com/?#Fragment')
-        self.assertEquals(uri.query, '')
+        self.assertEquals(str(uri.query), '')
 
-class FullUriParsing(unittest.TestCase):
     def test_full_uri_rfc_example(self):
         uri = URI('http://www.ics.uci.edu/pub/ietf/uri/#Related')
-        assert uri.scheme == 'http'
-        assert uri.authority == 'www.ics.uci.edu'
+        self.assertEquals(uri.scheme, 'http')
+        self.assertEquals(uri.authority, 'www.ics.uci.edu')
         self.assertEquals(uri.path, '/pub/ietf/uri/')
-        assert uri.fragment == 'Related'
+        self.assertEquals(uri.fragment, 'Related')
         self.assertEquals(uri.query, None)
